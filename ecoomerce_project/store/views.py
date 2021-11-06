9370a27f2f2f23f8e910f1de6587dbc3fb59d43f
Fantamady Sylla <fms294@nyu.edu>
2021-11-06 15:56:46 -0400

    add files from local repo

---
+++ ecoomerce_project/store/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product 
+
+def home(request, category_slug=None):
+    category_page = None
+    products = None
+    if category_slug!=None:
+        category_page = get_object_or_404(Category, slug=category_slug)
+        products =  Product.objects.filter(category=category_page, available=True) 
+    else:
+        products = Product.objects.all().filter(available=True)
+    return render (request, 'home.html', {'category': category_page, 'product': products})
+
+def productPage(request):
+    return render (request, 'product.html')
